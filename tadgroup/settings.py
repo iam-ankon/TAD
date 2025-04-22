f72fe13a6fb5dabbded17e659498aab172537f70
iam-ankon <111118106+iam-ankon@users.noreply.github.com>
2025-04-22 18:00:39 +0600

    Update settings.py

--- tadgroup/settings.py
+++ tadgroup/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
-
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -112,7 +112,7 @@ WSGI_APPLICATION = 'tadgroup.wsgi.application'
 # }
 
 DATABASES = {
-    'default':  {
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL')) {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'hrms',
         'USER': 'ankon',
